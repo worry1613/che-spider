804c7a925dcbe4957dec00d5b59137630506beab
wangrui <35493032@qq.com>
2019-09-19 01:55:23 +0800

    robots.txt

--- che/che/settings.py
+++ che/che/settings.py
@@ -19,7 +19,8 @@ NEWSPIDER_MODULE = 'che.spiders'
 #USER_AGENT = 'che (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
+# ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
