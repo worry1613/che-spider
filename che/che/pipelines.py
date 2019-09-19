6c4f64de591e0179fb33125eec2b0915b4071ba7
wangrui <35493032@qq.com>
2019-09-19 15:03:41 +0800

    debug

--- che/che/pipelines.py
+++ che/che/pipelines.py
@@ -5,9 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+
 
 class ChePipeline(object):
     def process_item(self, item, spider):
@@ -17,12 +15,12 @@ class ChePipeline(object):
         _tags = item.get('_keywords')
         _time = item.get('_datetime')
         _content = item.get('_content')
+        _text = item.get('_text')
         """
         数据写入文件
         """
-        f = '%d.txt' % (_writer,)
+        f = 'data/data.txt'
         fin = open(f,'a+')
-
-        fin.write('%d_|||_%s_|||_%s_|||_%s_|||_%s\n' % (_id,_title,_time,'|+|'.join(_tags),_content))
+        fin.write('%s_|||_%s_|||_%s_|||_%s_|||_%s_|||_%s\n' % (_id,_title,_time,'|+|'.join(_tags),_content,_text))
         fin.close()
         return ''
