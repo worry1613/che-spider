0903e818aec79def6b75b69ec13250fd811a26f9
wangrui <35493032@qq.com>
2019-09-19 15:03:09 +0800

    网上找的破解头条sign和翻页的代码

--- che/che/util.py
+++ che/che/util.py
@@ -1,47 +1,81 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-import json
-import hashlib
 import execjs
+import time
+import math
+import hashlib
+from functools import partial
+# from settings import signature_js_func,signature_js_path
+
+
+signature_js_path = 'javascript/signature.js'
+signature_js_func = 'get_sign'
+
+
+ascp_js_path = './javascript/ascp.js'
+ascp_js_func = 'ascp'
+
 
 def getresponsejson(response):
     if response._body[0] == '{' and response._body[-1] == '}':
         return response._body
     start = response._body.find('(') + 1
-    bodystr = response._body[start:-2]
+    bodystr = response._body[start:-1]
     # ret = json.loads(bodystr)
     return bodystr
 
-def getHoney(t):  #####根据JS脚本破解as ,cp
-    # t = int(time.time())  #获取当前时间
-    # t=1534389637
-    # print(t)
-    e = str('%X' % t)  ##格式化时间
-    # print(e)
-    m1 = hashlib.md5()  ##MD5加密
-    m1.update(str(t).encode(encoding='utf-8'))  ##转化格式
-    i = str(m1.hexdigest()).upper()  ####转化大写
-    # print(i)
-    n = i[0:5]  ##获取前5位
-    a = i[-5:]  ##获取后5位
+
+def get_js(js_file_path,mode='r'):
+
+    f = open(js_file_path,mode)
+    line = f.readline()
+    result = ''
+    while line:
+        result +=  line
+        line = f.readline()
+    return result
+
+def py_to_js(js_file_path,js_func,*params):
+
+    js_script = get_js(js_file_path)
+    JsContext = execjs.compile(js_script)
+    result = JsContext.call(js_func,*params)
+    return result
+
+def get_ascp():
+
+    t = int(math.floor(time.time()))
+    e = hex(t).upper()[2:]
+    m = hashlib.md5()
+    m.update(str(t).encode(encoding='utf-8'))
+    i = m.hexdigest().upper()
+    if len(e) != 8:
+        AS = '479BB4B7254C150'
+        CP = '7E0AC8874BB0985'
+        return AS,CP
+    n = i[0:5]
+    a = i[-5:]
     s = ''
     r = ''
-    for x in range(0, 5):
-        s += n[x] + e[x]
-        r += e[x + 3] + a[x]
-    eas = 'A1' + s + e[-3:]
-    ecp = e[0:3] + r + 'E1'
-    # print(eas)
-    # print(ecp)
-    return eas, ecp
+    for o in range(5):
+        s += n[o] + e[o]
+        r += e[o + 3] + a[o]
+    AS = 'A1' + s + e[-3:]
+    CP = e[0:3] + r + 'E1'
+    return AS,CP
 
-def get_js():
-    f = open(r"toutiao-TAC.sign", 'r')
-    line = f.readline()
-    htmlstr = ''
-    while line:
-        htmlstr = htmlstr + line
-        line = f.readline()
-    ctx = execjs.compile(htmlstr)
-    f.close()
-    return ctx.call('get_as_cp_signature')
+def payload_for_get( id, mode, max_behot_time):
+
+    _signature = py_to_js(signature_js_path, signature_js_func, id, max_behot_time)
+    # ascp = py_to_js(ascp_js_path,ascp_js_func)
+    _as,_cp = get_ascp()
+    return {
+        # 'page_type': mode,
+        # 'user_id': id,
+        # 'max_behot_time': max_behot_time,
+        # 'count':'20',
+        'as': _as,
+        'cp': _cp,
+        '_signature': _signature
+    }
+
+signature_func = partial(py_to_js,signature_js_path,signature_js_func)
+
